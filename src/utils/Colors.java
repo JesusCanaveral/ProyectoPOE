7539d9ac59ca8d39ec89de09b896463dce3706c3
Jesus <a21310397@ceti.mx>
2022-11-15 09:52:30 -0600

    avances colores y fuente

---
+++ src/utils/Colors.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package utils;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Colors {
+    public static Color white = new Color(255,255,255);
+    public static Color lightBlue = new Color(172,231,255);
+    public static Color mediumBlue = new Color(48,114,208);
+    public static Color darkBlue = new Color(9,21,64);
+    public static Color gray = new Color(68, 66, 66);
+    public static Color lightGreen = new Color(48, 122, 109);
+    public static Color darkGreen = new Color(48, 122, 109);
+}
---
+++ src/utils/Fonts.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package utils;
+
+import java.awt.Font;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Fonts{
+    public static Font title = new Font("Century", Font.PLAIN, 20);
+    public static Font normal = new Font("Century", Font.PLAIN, 14);
+}
